c276043eb837ff5ea70e62f040775126b3de47ea
Tye Rooth <tyerooth@gmail.com>
2021-11-22 22:04:22 -0500

    basic calculator designed, next displaying inputs

---
+++ script.js
@@ -0,0 +1,31 @@
+
+function operate (operator, a, b) {
+    if(operator === "+") {
+        add(a, b);
+    }
+    else if (operator === "-") {
+        subtract(a, b);
+    }
+    else if (operator === "x") {
+        multiply(a, b);
+    }
+    else if (operator === "/") {
+        divide(a, b);
+    }
+}
+
+function add(a, b) {
+    return a + b;
+};
+
+function subtract(a, b) {
+    return a - b;
+};
+
+function multiply(a, b) {
+    return a * b;
+};
+
+function divide(a, b) {
+    return a / b;
+};
