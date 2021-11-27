30b66e4515eb98b47cf4e3e6e8176b2f29b5a1f6
Tye Rooth <tyerooth@gmail.com>
2021-11-26 22:05:50 -0500

    Added Error Checking to Calculator

--- script.js
+++ script.js
@@ -1,13 +1,43 @@
 const display = document.querySelector(".display");
 
+// Array of operators is important for error checking function
+const operatorsList = ["/", "x", "-", "+"];
+
 const buttons = document.querySelectorAll(".btn");
 buttons.forEach(button => {
-    button.addEventListener('click', addToDisplay)
+    button.addEventListener('click', () => {
+        display.textContent += `${ button.id }`;
+    });
 });
 
-function addToDisplay(symbol) {
-    display.textContent = `${ symbol.target.id }`;
-}
+const equals = document.querySelector("#equals");
+equals.addEventListener('click', equate);
+
+function equate () {
+    equation = display.textContent;
+    equationArray = equation.split("");
+    if (errorCheck(equationArray) === true) {
+        display.textContent = "ERROR";
+    }
+    console.log(`${ equationArray }`);
+};
+
+function errorCheck (eq) {   
+    if (operatorsList.includes(eq[0]) === true) {
+        return true;
+    }
+    else if (operatorsList.includes(eq[eq.length - 1]) === true) {
+        return true;
+    } 
+    // Check for double operators
+    for (let i = 1; i < eq.length - 1; i++) {
+        if (operatorsList.includes(eq[i]) === true) {
+            if (operatorsList.includes(eq[i - 1]) || operatorsList.includes(eq[i + 1])) {
+                return true;
+            }
+        }
+    }
+};
 
 function operate (operator, a, b) {
     if(operator === "+") {
